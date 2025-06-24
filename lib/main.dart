db5563104d448a063f8987f66e8cfbe1de9fb3b7
fabriapp <39033189+luisfernandosolis@users.noreply.github.com>
2025-06-24 11:22:25 -0500

    changes in a main file

--- lib/main.dart
+++ lib/main.dart
@@ -3,95 +3,187 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-//             icon: const Icon(Icons.login),
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Fabriapps Academy',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.grey,
+        brightness: Brightness.dark,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _currentIndex = 0;
 
-  void _incrementCounter() {
+  void onTabTapped(int index) {
     setState(() {
-      _counter++;
+      _currentIndex = index;
     });
+  }
+
+  Widget _buildProfileTab(BuildContext context) {
+    return Center(
+      child: IconButton(
+        icon: Icon(Icons.person),
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('Perfil de Usuario'),
+                content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text('Nombre: Usuario'),
+                    Text('Correo: usuario@correo.com'),
+                    Image.network('https://url-de-la-imagen.com'),
+                  ],
+                ),
+                actions: <Widget>[
+                  TextButton(
+                    child: Text('Cerrar'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        },
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blue,
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
+    final List<Widget> _children = [
+      Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Productos relacionados a cursos de Inteligencia Artificial',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            DataTable(
+              columns: const <DataColumn>[
+                DataColumn(
+                  label: Text(
+                    'Producto',
+                    style: TextStyle(fontStyle: FontStyle.italic),
+                  ),
+                ),
+                DataColumn(
+                  label: Text(
+                    'Precio',
+                    style: TextStyle(fontStyle: FontStyle.italic),
+                  ),
+                ),
+                DataColumn(
+                  label: Text(
+                    'Ver más',
+                    style: TextStyle(fontStyle: FontStyle.italic),
+                  ),
+                ),
+              ],
+              rows: <DataRow>[
+                DataRow(
+                  cells: <DataCell>[
+                    DataCell(Text('Curso básico de IA')),
+                    DataCell(Text('\$100')),
+                    DataCell(IconButton(icon: Icon(Icons.arrow_forward), onPressed: () {})),
+                  ],
+                ),
+                DataRow(
+                  cells: <DataCell>[
+                    DataCell(Text('Curso avanzado de IA')),
+                    DataCell(Text('\$200')),
+                    DataCell(IconButton(icon: Icon(Icons.arrow_forward), onPressed: () {})),
+                  ],
+                ),
+                DataRow(
+                  cells: <DataCell>[
+                    DataCell(Text('Curso de IA aplicada')),
+                    DataCell(Text('\$300')),
+                    DataCell(IconButton(icon: Icon(Icons.arrow_forward), onPressed: () {})),
+                  ],
+                ),
+              ],
             ),
             ElevatedButton(
+              child: Text('Login'),
               onPressed: () {
-                Navigator.pushNamed(context, '/login');
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPage()),
+                );
               },
-              child: const Text('Go to Login Page'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      _buildProfileTab(context),
+      Center(
+        child: Text(
+          'Ajustes',
+          style: TextStyle(fontSize: 30),
+        ),
       ),
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Fabriapps Academy'),
+        leading: Image.asset('assets/logo.png'),
+      ),
+      body: _children[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
+        items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
-            label: 'Home',
+            label: 'Inicio',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: 'Favorites',
+            icon: Icon(Icons.person),
+            label: 'Perfil',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Ajustes',
           ),
         ],
-        onTap: (index) {
-          switch (index) {
-            case 0:
-              Navigator.pushNamed(context, '/home');
-              break;
-            case 1:
-              Navigator.pushNamed(context, '/favorites');
-              break;
-          }
-        },
+      ),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Center(
+        child: Text('Página de Login'),
       ),
     );
   }
